aa46c75bed5ff11d40ed92150bcb1d3ab56e3281
Lu Guzman <luguzman@frba.utn.edu.ar>
2026-08-23 19:20:08 -0300

    Agrega comentario explicativo en multiplicar

--- labs/lab-github/operaciones.c
+++ labs/lab-github/operaciones.c
@@ -13,7 +13,7 @@ int restar(int a, int b) {
 }
 
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
-
+/* Recibe dos enteros como parametros y devuelve la multiplicacion entre ellos*/
 int multiplicar(int a, int b) {
     return a * b; /* TODO: implementar */
 }
