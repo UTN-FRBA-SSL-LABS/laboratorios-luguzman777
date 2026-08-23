7d9a4c55ec70408d3fad556eab19001d6b9ba56d
Lu Guzman <luguzman@frba.utn.edu.ar>
2026-08-23 20:35:17 -0300

    Implementa multiplicar correctamente

--- labs/lab-github/operaciones.c
+++ labs/lab-github/operaciones.c
@@ -15,7 +15,7 @@ int restar(int a, int b) {
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
 /* Recibe dos enteros como parametros y devuelve la multiplicacion entre ellos*/
 int multiplicar(int a, int b) {
-    return a * b; /* TODO: implementar */
+    return a * b;
 }
 
 /* ── esPar ────────────────────────────────────────────────────────────────── */
